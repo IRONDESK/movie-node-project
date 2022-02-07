08d9952cc945c22d9c89f9913fc5699faa5dc15a
SOOCHUL SON <zerozerobase@gmail.com>
2022-02-07 23:59:57 +0900

    feat: 글쓰기 창 디자인 변경

--- resource/static/js/scripts.js
+++ resource/static/js/scripts.js
@@ -12,8 +12,10 @@ function GetNaverMovieImg(keyword){
 
 
 const formContainer = document.querySelector(".form-container");
-const searchBtn = formContainer.querySelector(".form-style .searchBtn");
+const searchBtn = formContainer.querySelector(".form-style .search-btn");
+const rateBtnCont = formContainer.querySelector(".rate-wrap .rate-btn")
 const titleInput = formContainer.querySelector(".form-style #title");
+const ratingInput = formContainer.querySelector(".form-style #rating");
 const imglinkInput = formContainer.querySelector(".form-style #imagelink");
 const imglinkCont = formContainer.querySelector(".imagelink-list ul");
 const SelectedImgPlace = formContainer.querySelector(".selected-img-place");
@@ -31,13 +33,12 @@ function findNaverMovie () {
     type="radio"
     id="item-No${index}"
     name="result-movie"
-    
     data-title="${titleChange}"
     data-imglink="${item.image}"
+    hidden
 ></input>
 <label
     for="item-No${index}"
-
 >
                 <b>${titleChange}</b> (${item.pubDate})<br>
                 ${item.director.split("|")[0]}<br>
@@ -48,13 +49,19 @@ function findNaverMovie () {
     imglinkCont.innerHTML = searchResult;
 }
 
+rateBtnCont.addEventListener("click", (e) => {
+    if (e.target.dataset.rate !== undefined) {
+        ratingInput.value = e.target.dataset.rate;
+    }
+});
+
 imglinkCont.addEventListener("click", (e) => {
     if (e.target.dataset.title !== undefined) {
         titleInput.value = e.target.dataset.title;
         imglinkInput.value = e.target.dataset.imglink;
         SelectedImgPlace.innerHTML = `<img src="${e.target.dataset.imglink}" class="selected-movie-img">`;
     }
-})
+});
 
 
 // tinymce.init({selector:'textarea'});
